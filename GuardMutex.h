426520f65907fe4cd6c2c1f5fcf98cb754e70ce9
Thomas Chalker <tomchalker@gmail.com>
2015-03-02 23:05:52 -0500

    forgot to add guard

---
+++ GuardMutex.h
@@ -0,0 +1,23 @@
+#ifndef GUARDMUTEX_H
+#define GUARDMUTEX_H
+
+#include <SDL2/SDL.h>
+
+class GuardMutex
+{
+public:
+    explicit GuardMutex(SDL_mutex *pMutex):
+        m_pMutex(pMutex)
+        {
+            SDL_LockMutex(m_pMutex);
+        }
+    ~GuardMutex()
+        {
+            SDL_UnlockMutex(m_pMutex);
+        }
+
+private:
+    SDL_mutex *m_pMutex;
+};
+
+#endif
